5434299d1828833919037812d83fce3772e1e8b6
huashan2019 <411736371@qq.com>
2019-10-31 18:45:02 +0800

    delete app files

--- KEA128_DSP_APP/Keil_Pro/Output/startup_skeaz1284.d
+++
@@ -1,1 +0,0 @@
-.\output\startup_skeaz1284.o: RTE\Device\SKEAZ128xxx4\startup_SKEAZ1284.s
--- KEA128_DSP_APP/Source/App/app_tdf8541.h
+++
@@ -1,6 +0,0 @@
-///=========================
-#ifndef __APP_TDF8541_H
-#define __APP_TDF8541_H
-
-extern void Amp8541_DLL_Loader(void);
-#endif
